f1f27fc6332ce352958613fdd483b6be08127980
Aleksandar Stojanovic <astojanovic17@raf.rs>
2019-10-19 14:33:23 +0200

    login page added
    added login support
    restructured ejs pages
    fixed root issues

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const posts = [];
 //app setup
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
 
@@ -28,12 +28,12 @@ app.listen(4000, () => {
 
 //home
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render("auth",{css: css});
 });
 
 //feed
 app.get('/news', (req,res) => {
-    res.render('board')
+    res.render('board',{posts: posts, css: css});
 });
 
 //retrive user profiles
@@ -66,6 +66,11 @@ app.post('/profiles', (req, res) => {
     console.log(profile);
     profiles.push(profile);
     res.status(200).json(profile);
+
+    const userPosts = posts.filter((post)=>post.id==req.params.id);
+    console.log(JSON.stringify(userPosts));
+    res.render('profile', {profile: profiles[req.params.id], posts: userPosts, css: css});
+
 });
 
 //post a post
